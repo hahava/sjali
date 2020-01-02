51eac1aae3c906471f033fc66b681e7bbabb2e51
hahava <hahava5696@gmail.com>
2020-01-03 00:16:17 +0900

    Refactor annonymous class to lambda expression

--- sejong_Alli/app/src/main/java/com/example/eeww9/sejong_alli/Splash.java
+++ sejong_Alli/app/src/main/java/com/example/eeww9/sejong_alli/Splash.java
@@ -5,20 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ * 첫 화면시 나타나는 splash view
+ *
+ * @author hahava
+ */
 public class Splash extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        Handler hd = new Handler();
-        hd.postDelayed(new splashhandler(), 5000); // 5초 후에 hd Handler 실행
-    }
+	//5 seconds
+	private final int DELAYED_SECONDS = 5000;
 
-    private class splashhandler implements Runnable {
-        public void run() {
-            startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩이 끝난후 이동할 Activity
-            Splash.this.finish(); // 로딩페이지 Activity Stack에서 제거
-        }
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(() -> {
+			startActivity(new Intent(getApplication(), MainActivity.class)); // 로딩이 끝난후 이동할 Activity
+			finish();
+		}, DELAYED_SECONDS);
+	}
+
 }
