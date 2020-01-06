0ac45e000442f9eb2f12dc59254078fb93d3a3f7
hahava <hahava5696@gmail.com>
2020-01-06 22:46:31 +0900

    Refactor coding convention

--- sejong_Alli/app/src/main/java/com/example/eeww9/sejong_alli/fragment/Tab/InSide.java
+++ sejong_Alli/app/src/main/java/com/example/eeww9/sejong_alli/fragment/Tab/InSide.java
@@ -10,24 +10,24 @@ import com.example.eeww9.sejong_alli.CustomListviewAdapter;
 import com.example.eeww9.sejong_alli.R;
 
 public class InSide extends Fragment {
-	private View view;
-	private ListView MenuList;
-	private CustomListviewAdapter ListViewAdapter;
+	private View tabInnerView;
+	private ListView items;
+	private CustomListviewAdapter itemListViewApdapter;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		if (view == null) {
-			view = inflater.inflate(R.layout.in_side, null);
+		if (tabInnerView == null) {
+			tabInnerView = inflater.inflate(R.layout.in_side, null);
 		}
 
 		//Example
-		MenuList = (ListView)view.findViewById(R.id.inList);
-		ListViewAdapter = new CustomListviewAdapter(getActivity());
-		MenuList.setAdapter(ListViewAdapter);
+		items = (ListView)tabInnerView.findViewById(R.id.inList);
+		itemListViewApdapter = new CustomListviewAdapter(getActivity());
+		items.setAdapter(itemListViewApdapter);
 
-		ListViewAdapter.addItem(getResources().getDrawable(R.drawable.out_one, null),
+		itemListViewApdapter.addItem(getResources().getDrawable(R.drawable.out_one, null),
 			"스토리, 세대를 잇다!",
 			"~ 2016_12_30");
 
-		return view;
+		return tabInnerView;
 	}
 }
