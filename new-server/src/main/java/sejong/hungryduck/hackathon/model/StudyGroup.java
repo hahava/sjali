c4fd8a839c581cf9e9d24a8be9774e2159e0c1c5
hahava <hahava5696@gmail.com>
2020-01-22 22:55:41 +0900

    Add model study group

---
+++ new-server/src/main/java/sejong/hungryduck/hackathon/model/StudyGroup.java
@@ -0,0 +1,33 @@
+package sejong.hungryduck.hackathon.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+public class StudyGroup {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+	private String title;
+	private String subject;
+	private String personel;
+	private String contents;
+	private String name;
+	private String major;
+	private String cellPhone;
+
+	@CreationTimestamp
+	private LocalDateTime creationTime;
+
+	@UpdateTimestamp
+	private LocalDateTime updateTime;
+}
---
+++ sejong_Alli/app/src/main/java/com/sejong/hungryduck/model/StudyGroup.java
@@ -0,0 +1,68 @@
+package com.sejong.hungryduck.model;
+
+public class StudyGroup {
+
+	private String title;
+	private String subject;
+	private String personel;
+	private String contents;
+	private String name;
+	private String major;
+	private String cellPhone;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getPersonel() {
+		return personel;
+	}
+
+	public void setPersonel(String personel) {
+		this.personel = personel;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+}
