7973a205cf60a32d578baa167cadba0cce8d27f9
hahava <hahava5696@gmail.com>
2020-01-23 20:27:52 +0900

    Add post method validation

--- new-server/src/main/java/sejong/hungryduck/hackathon/controller/StudyGroupController.java
+++ new-server/src/main/java/sejong/hungryduck/hackathon/controller/StudyGroupController.java
@@ -3,12 +3,18 @@ package sejong.hungryduck.hackathon.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import sejong.hungryduck.hackathon.model.Pagination;
 import sejong.hungryduck.hackathon.model.StudyGroup;
 import sejong.hungryduck.hackathon.persistence.StudyGroupRepository;
 
+import javax.validation.Valid;
 import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @AllArgsConstructor
@@ -31,8 +37,12 @@ public class StudyGroupController {
 	}
 
 	@PostMapping("/studyGroup")
-	public void addStudyGroup(StudyGroup studyGroup) {
+	public ResponseEntity<String> addStudyGroup(@Valid StudyGroup studyGroup, BindingResult br) {
+		if (br.hasErrors()) {
+			return new ResponseEntity<>("Not valid params", BAD_REQUEST);
+		}
 		studygroupRepository.save(studyGroup);
+		return new ResponseEntity<>("register success", OK);
 	}
 
 }
