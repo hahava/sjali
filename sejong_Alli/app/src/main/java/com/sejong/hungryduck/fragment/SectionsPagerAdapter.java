ea0f8fced8cbaedb01a8fbb8863fa664592f3f8c
hahava <hahava5696@gmail.com>
2020-02-17 21:46:47 +0900

    Change tab count three to two

--- sejong_Alli/app/src/main/java/com/sejong/hungryduck/fragment/SectionsPagerAdapter.java
+++ sejong_Alli/app/src/main/java/com/sejong/hungryduck/fragment/SectionsPagerAdapter.java
@@ -5,7 +5,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import com.sejong.hungryduck.fragment.Tab.FirstView;
 import com.sejong.hungryduck.fragment.Tab.SecondView;
-import com.sejong.hungryduck.fragment.Tab.ThirdView;
 import com.sejong.hungryduck.model.TabMetaInfo;
 
 import java.util.HashMap;
@@ -22,8 +21,7 @@ public class SectionsPagerAdapter extends FragmentPagerAdapter {
 	private static Map<Integer, TabMetaInfo> setPageTitle() {
 		Map<Integer, TabMetaInfo> pageTitle = new HashMap<>();
 		pageTitle.put(0, new TabMetaInfo("외부", new FirstView()));
-		pageTitle.put(1, new TabMetaInfo("교내", new SecondView()));
-		pageTitle.put(2, new TabMetaInfo("소모임", new ThirdView()));
+		pageTitle.put(1, new TabMetaInfo("소모임", new SecondView()));
 		return pageTitle;
 	}
 
--- sejong_Alli/app/src/main/java/com/sejong/hungryduck/fragment/Tab/SecondView.java
+++ sejong_Alli/app/src/main/java/com/sejong/hungryduck/fragment/Tab/SecondView.java
@@ -1,31 +1,40 @@
 package com.sejong.hungryduck.fragment.Tab;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-import com.sejong.hungryduck.viewadapter.PostingsListviewAdapter;
+import com.sejong.hungryduck.activity.WriteGroup;
 import com.sejong.hungryduck.sejong.R;
+import com.sejong.hungryduck.viewadapter.PostingsListviewAdapter;
 
 public class SecondView extends Fragment {
+
 	private View tabInnerView;
-	private ListView items;
-	private PostingsListviewAdapter itemListViewAdapter;
+	private ListView groups;
+	private PostingsListviewAdapter listViewAdapter;
+	private FloatingActionButton groupAddBtn;
 
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		if (tabInnerView == null) {
-			tabInnerView = inflater.inflate(R.layout.in_side, null);
+			tabInnerView = inflater.inflate(R.layout.group, null);
 		}
 
-		setItemListView();
+		groups = (ListView)tabInnerView.findViewById(R.id.groupList);
+		listViewAdapter = new PostingsListviewAdapter();
+		groups.setAdapter(listViewAdapter);
+
+		groupAddBtn = (FloatingActionButton)tabInnerView.findViewById(R.id.fab);
+		groupAddBtn.setOnClickListener(v -> {
+			Intent intent = new Intent(v.getContext(), WriteGroup.class);
+			startActivity(intent);
+		});
+
 		return tabInnerView;
 	}
 
-	private void setItemListView() {
-		items = (ListView)tabInnerView.findViewById(R.id.inList);
-		itemListViewAdapter = new PostingsListviewAdapter();
-		items.setAdapter(itemListViewAdapter);
-	}
 }
