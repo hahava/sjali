f18c447bf39a18ec702fbedaf14adb1e1882f417
hahava <hahava5696@gmail.com>
2020-06-03 22:17:12 +0900

    Add databinding in main activity

--- sejong_Alli/app/src/main/java/com/sejong/hungryduck/activity/MainActivity.java
+++ sejong_Alli/app/src/main/java/com/sejong/hungryduck/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sejong.hungryduck.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,23 +8,28 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.sejong.hungryduck.activity.fragment.SectionsPagerAdapter;
 import com.sejong.hungryduck.sejong.R;
+import com.sejong.hungryduck.sejong.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
 	private ViewPager mViewPager;
 	private TabLayout tabLayout;
+	private ActivityMainBinding dataBindingUtil;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		viewBinding();
+		init();
+
 		mViewPager.setAdapter(new SectionsPagerAdapter(getSupportFragmentManager()));
 		tabLayout.setupWithViewPager(mViewPager);
 	}
 
-	public void viewBinding() {
-		mViewPager = (ViewPager)findViewById(R.id.container);
-		tabLayout = (TabLayout)findViewById(R.id.tab_view);
+	public void init() {
+		dataBindingUtil = DataBindingUtil.setContentView(this, R.layout.activity_main);
+		mViewPager = dataBindingUtil.container;
+		tabLayout = dataBindingUtil.tabView;
 	}
+
 }
