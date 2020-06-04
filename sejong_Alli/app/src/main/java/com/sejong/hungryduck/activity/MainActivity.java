88f5c188817ca807f873051bb0e201b194b2ce92
hahava <hahava5696@gmail.com>
2020-06-04 23:52:37 +0900

    Bump up android sdk 24 to 28

--- sejong_Alli/app/src/main/java/com/sejong/hungryduck/activity/MainActivity.java
+++ sejong_Alli/app/src/main/java/com/sejong/hungryduck/activity/MainActivity.java
@@ -1,10 +1,10 @@
 package com.sejong.hungryduck.activity;
 
-import android.databinding.DataBindingUtil;
+import androidx.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.sejong.hungryduck.activity.fragment.SectionsPagerAdapter;
 import com.sejong.hungryduck.sejong.R;
